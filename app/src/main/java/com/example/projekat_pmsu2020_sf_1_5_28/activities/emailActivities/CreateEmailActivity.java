6dc5e413debddf8760b2f300089ad42c3d6ffff1
zoricNikola <nikola.se.zoric@gmail.com>
2020-06-08 16:24:13 +0200

    Kreirane sledece aktivnosti
    CreateEmailActivity, EmailsActivity, FolderActivity, SettingsActivity

---
+++ app/src/main/java/com/example/projekat_pmsu2020_sf_1_5_28/activities/emailActivities/CreateEmailActivity.java
@@ -0,0 +1,7 @@
+package com.example.projekat_pmsu2020_sf_1_5_28.activities.emailActivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateEmailActivity extends AppCompatActivity {
+
+}
---
+++ app/src/main/java/com/example/projekat_pmsu2020_sf_1_5_28/activities/settingActivities/SettingsActivity.java
@@ -0,0 +1,7 @@
+package com.example.projekat_pmsu2020_sf_1_5_28.activities.settingActivities;
+
+import android.preference.PreferenceActivity;
+
+public class SettingsActivity extends PreferenceActivity {
+
+}
