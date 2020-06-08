1f7e8c9367d609d6430304ad00dd64de77695701
Stefan <kockar.stefan@gmail.com>
2020-06-08 16:53:28 +0200

    Kreirane profile, login, contact i contacts aktivnosti

---
+++ app/src/main/java/com/example/projekat_pmsu2020_sf_1_5_28/activities/profile/ProfileActivity.java
@@ -0,0 +1,6 @@
+package com.example.projekat_pmsu2020_sf_1_5_28.activities.profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileActivity extends AppCompatActivity {
+}
