4cffbe343f867d90ed6679ca3a30972b97a884f6
Stefan <kockar.stefan@gmail.com>
2020-06-08 17:04:09 +0200

    Redefinisane osnovne metode zivotnog ciklusa aktivnosti

--- app/src/main/java/com/example/projekat_pmsu2020_sf_1_5_28/activities/contactActivities/ContactActivity.java
+++ app/src/main/java/com/example/projekat_pmsu2020_sf_1_5_28/activities/contactActivities/ContactActivity.java
@@ -1,6 +1,37 @@
 package com.example.projekat_pmsu2020_sf_1_5_28.activities.contactActivities;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ContactActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 }
