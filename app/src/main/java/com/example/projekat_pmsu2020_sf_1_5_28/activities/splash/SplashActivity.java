c51363be16f8bc337b35a955decea1b255610066
Boris <jankovicb0230@gmail.com>
2020-06-08 17:34:06 +0200

    Implementiran splash

--- app/src/main/java/com/example/projekat_pmsu2020_sf_1_5_28/activities/splash/SplashActivity.java
+++ app/src/main/java/com/example/projekat_pmsu2020_sf_1_5_28/activities/splash/SplashActivity.java
@@ -1,16 +1,41 @@
 package com.example.projekat_pmsu2020_sf_1_5_28.activities.splash;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.projekat_pmsu2020_sf_1_5_28.R;
+import com.example.projekat_pmsu2020_sf_1_5_28.activities.login.LoginActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        if(isOnline()){
+            int splashTime = 1000;
+
+            new Timer().schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }, splashTime);
+        }else{
+            Toast.makeText(this, "No network connection",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    protected boolean isOnline(){
+        return true;
     }
 
     @Override
