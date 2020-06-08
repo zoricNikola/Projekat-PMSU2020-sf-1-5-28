4595ca041af380adbf4e72c48511536a6ac36142
zoricNikola <nikola.se.zoric@gmail.com>
2020-06-08 17:04:35 +0200

    Overridovane metode zivotnog ciklusa aktivnosti

--- app/src/main/java/com/example/projekat_pmsu2020_sf_1_5_28/activities/settingActivities/SettingsActivity.java
+++ app/src/main/java/com/example/projekat_pmsu2020_sf_1_5_28/activities/settingActivities/SettingsActivity.java
@@ -1,7 +1,38 @@
 package com.example.projekat_pmsu2020_sf_1_5_28.activities.settingActivities;
 
+import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
 public class SettingsActivity extends PreferenceActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 
 }
