7c67046c6fc1c9dab790f018b8dade47f2a93db8
Boris <jankovicb0230@gmail.com>
2020-06-08 17:06:53 +0200

    Napravljenje aktivnosti: SplashActivity, FoldersActivity,EmailActivity, CreateContactActivity

--- app/src/main/java/com/example/projekat_pmsu2020_sf_1_5_28/activities/splash/SplashActivity.java
+++ app/src/main/java/com/example/projekat_pmsu2020_sf_1_5_28/activities/splash/SplashActivity.java
@@ -1,6 +1,38 @@
 package com.example.projekat_pmsu2020_sf_1_5_28.activities.splash;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 }
