8b7de5ee0a8e3c0195172897b06b19796825f95e
zoricNikola <nikola.se.zoric@gmail.com>
2020-06-13 15:30:06 +0200

    Kreiran MainActivity
    Ova aktivnost ce biti glavna aktivnost aplikacije. Sadrzi toolbar i navigation view. Takodje sadrzi fragmentHolder u koji ce se smestati fragmenti za prikaz listi poruka, foldera i kontakata.

---
+++ app/src/main/java/com/example/projekat_pmsu2020_sf_1_5_28/activities/MainActivity.java
@@ -0,0 +1,137 @@
+package com.example.projekat_pmsu2020_sf_1_5_28.activities;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.example.projekat_pmsu2020_sf_1_5_28.R;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private Toolbar mToolbar;
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
+    private boolean appStarting;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setToolbar();
+        setNavigationDrawer();
+
+        appStarting = true;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        navigationItemClicked(R.id.item_emails);
+    }
+
+    private void setToolbar() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);
+        getSupportActionBar().setTitle(null);
+    }
+
+    private void setNavigationDrawer() {
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mNavigationView = (NavigationView) findViewById(R.id.navigationView);
+        mNavigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        navigationItemClicked(item.getItemId());
+
+        return true;
+    }
+
+    private void navigationItemClicked(int itemId) {
+
+        MenuItem item = mNavigationView.getMenu().findItem(itemId);
+
+        if (!item.isChecked()) {
+            if (appStarting)
+                item.setChecked(true);
+            Toast.makeText(MainActivity.this, item.getTitle(), Toast.LENGTH_LONG).show();
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+
+            switch (itemId) {
+                case R.id.item_emails:
+                    startEmailsFragment();
+                    break;
+                case R.id.item_folders:
+                    // folders
+                    break;
+                case R.id.item_contacts:
+                    // contacts
+                    break;
+                case R.id.item_settings:
+                    // settings
+                    break;
+            }
+        }
+        else {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        }
+
+    }
+
+    private void startEmailsFragment() {
+        appStarting = false;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            case R.id.item_search:
+                Toast.makeText(MainActivity.this, "Search clicked", Toast.LENGTH_LONG).show();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START))
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        else
+            super.onBackPressed();
+    }
+
+    public void onUserAvatarClick(View view) {
+        Toast.makeText(MainActivity.this, "User avatar clicked", Toast.LENGTH_LONG).show();
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+    }
+}
