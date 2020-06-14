d0d62cf6cbd3cf3d6091ec21cdc1e98d7b84c239
Stefan <kockar.stefan@gmail.com>
2020-06-14 16:22:06 +0200

    Implentiran Login ekran

--- app/src/main/java/com/example/projekat_pmsu2020_sf_1_5_28/activities/login/LoginActivity.java
+++ app/src/main/java/com/example/projekat_pmsu2020_sf_1_5_28/activities/login/LoginActivity.java
@@ -1,17 +1,31 @@
 package com.example.projekat_pmsu2020_sf_1_5_28.activities.login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.projekat_pmsu2020_sf_1_5_28.R;
 
+import com.example.projekat_pmsu2020_sf_1_5_28.activities.MainActivity;
+import com.google.android.material.textfield.TextInputEditText;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private TextInputEditText mUsernameInput, mPasswordInput;
+    private Button mLoginButton, mRegisterButton;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mUsernameInput = findViewById(R.id.usernameInput);
+        mPasswordInput = findViewById(R.id.passwordInput);
+        mLoginButton = findViewById(R.id.loginButton);
+        mRegisterButton = findViewById(R.id.registerButton);
     }
 
     @Override
@@ -27,6 +41,18 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        mLoginButton.setOnClickListener(new View.OnClickListener()  {
+           @Override
+           public void onClick(View v) {
+               startMainActivity();
+           }
+        });
+    }
+
+    private void startMainActivity() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 
     @Override
